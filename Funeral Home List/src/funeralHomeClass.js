ea987f537602cbc61d2cdc9529127981ce8a7ad2
Chris Waters <chriswatersaudio@gmail.com>
2023-10-19 13:05:06 -0400

    added class parameters

--- Funeral Home List/src/funeralHomeClass.js
+++ Funeral Home List/src/funeralHomeClass.js
@@ -1,3 +1,20 @@
-class funeralHome {
-    
+class FuneralHome {
+    constructor(name, cityState, streetAddress, phone, email, website, zip) {
+        this.name = name;
+        this.cityState = cityState;
+        this.streetAddress = streetAddress;
+        this.phone = phone;
+        this.email = email;
+        this.website = website;
+        this.zip = zip;
+    }
+
 }
+
+// name: "",
+        // cityState: "",
+        // address: "",
+        // phone: "",
+        // email: "",
+        // website: "",
+        // zip: ""
